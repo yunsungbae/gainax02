b3cf45ecd6e4ef410fc167370e715278c103ce5f
baeyunsung <gainax02@gmail.com>
2018-10-04 17:21:20 +0900

    fitst commit

---
+++ src/main/java/com/gainax/gainax/GainaxApplication.java
@@ -0,0 +1,12 @@
+package com.gainax.gainax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GainaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GainaxApplication.class, args);
+    }
+}
